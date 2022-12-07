608b5bc71437bd32c6971d82d40dc78e169379f3
LPandorf <leticiapandorf@gmail.com>
2022-12-07 20:01:42 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Navbar from "./components/Navbar";
+import styled from "styled-components";
 
 export default function App() {
   return (
     <>
       <Navbar/>
       <Wrapper>
-        
+
       </Wrapper>
     </>
   );
 }
+
+const Wrapper=styled.div`
+
+`;
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export default function Navbar(){
+    return (
+        <Header>
+            CINEFLEX
+        </Header>
+    );
+}
+
+const Header=styled.div`
+    height: 67px;
+    left: 0px;
+    top: 0px;
+    background: #C3CFD9;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 34px;
+    line-height: 40px;
+    color: #E8833A;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
