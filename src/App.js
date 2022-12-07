beb7f7b4c04a019bf9ec8de43f5ecdb1d49fd4ce
LPandorf <leticiapandorf@gmail.com>
2022-12-07 20:53:27 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import styled from "styled-components";
+import Home from "./components/Home";
+import Sessions from "./components/Sessions";
+import Seats from "./components/Seats";
+import Success from "./components/Success";
 
 export default function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Wrapper>
-
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/sessoes/:idFilme" element={<Sessions/>} />
+            <Route path="/assentos/:idSessao" element={<Seats/>} />
+            <Route path="/sucesso" element={<Success/>} />
+          </Routes>
+        </BrowserRouter>
       </Wrapper>
     </>
   );
 }
 
-const Wrapper=styled.div`
-
+const Wrapper = styled.div`
+  margin-top: 67px;
 `;
