6adb3cad55ebb15b5a61dee0aaa1ac9531faafab
LPandorf <leticiapandorf@gmail.com>
2022-12-07 21:11:44 -0300

    home

--- src/App.js
+++ src/App.js
@@ -25,5 +25,5 @@ export default function App() {
 }
 
 const Wrapper = styled.div`
-  margin-top: 67px;
+  margin-top: 47px;
 `;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+
 export default function Home(){
     return (
-        <div>Home</div>
+        <Title>Selecione o filme</Title>
     );
 }
+
+const Title= styled.div`
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 28px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    letter-spacing: 0.04em;
+    color: #293845;
+`;
