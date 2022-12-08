64c7be52c095e12f503156dd920fb41565ec63f6
LPandorf <leticiapandorf@gmail.com>
2022-12-08 17:43:58 -0300

    pagina de selecionar o horario

---
+++ src/components/Sessions.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import {URL} from "../Constants";
+import Session from "./childcomponents/Session";
+
+export default function Sessions(){
+    const {idFilme}= useParams();
+    const [movie, setMovie]= useState(undefined);
+
+    useEffect(()=>{
+        const promise=axios.get(`${URL}/movies/${idFilme}/showtimes`);
+        promise.then(res=>setMovie(res.data));
+        promise.catch(err=>console.log(err.response.data));
+    })
+
+    if(!movie){
+        return (
+            <Loading>Carregando ...</Loading>
+        );
+    }
+
+    return (
+        <>
+            <Title>Selecione o horário</Title>
+            <Times>
+                {movie.days.map(m=>(
+                    <Session movie={m} key={m.id} />
+                ))}
+            </Times>
+        </>
+    )
+}
+
+const Loading= styled.div`
+    display: flex;
+    justify-content: center;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 28px;
+`;
+const Title= styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 28px;
+    letter-spacing: 0.04em;
+    color: #293845;
+    margin-bottom: 20px;
+`;
+const Times=styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+`;
---
+++ src/components/childcomponents/Session.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export default function Session({movie}){
+    return (
+        <Weekday>
+            {movie.weekday} - {movie.date}
+            <Time>
+                {movie.showtimes.map((t)=>(
+                    <Link key={t.name} to={`/assentos/${t.id}`} >
+                        <Button>{t.name}</Button>
+                    </Link>
+                ))}
+            </Time>
+        </Weekday>
+    );
+}
+
+const Weekday=styled.div`
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 23px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    letter-spacing: 0.02em;
+    color: #293845;
+    width: 100%;
+    margin: 23px;
+`
+const Time = styled.div`
+    display: flex;
+`
+const Button = styled.div`
+    width: 83px;
+    height: 43px;
+    background: #E8833A;
+    border-radius: 3px;
+    margin-right: 8px;
+    margin-top: 20px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+    letter-spacing: 0.02em;
+    color: #FFFFFF;
+`
